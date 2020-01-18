fa6abcb84909090d813c69a21d4ed69ee5458165
Geghi <mantovani.giacomo96@gmail.com>
2020-01-18 11:49:31 +0100

    basic java code ( GUI, MongoManager )

---
+++ DMMLProject/target/classes/application/Tweet.java
@@ -0,0 +1,21 @@
+package main.resources.application;
+
+public class Tweet {
+	private String text;
+
+	public Tweet(String text) {
+		this.text = text;
+	}
+	
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	
+}
+
+
